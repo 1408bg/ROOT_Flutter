653e7607486f93cbcc79458418f443b62ad876a9
taejun <ptj01036251@gmail.com>
2025-01-07 10:52:31 +0900

    feat: 앱에서 사용되는 색 지정

---
+++ lib/components/root_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class RootColors {
+  static Color gray100 = const Color(0xffFFFFFF);
+  static Color gray200 = const Color(0xffE4E6EB);
+  static Color gray300 = const Color(0xffC6C8CC);
+  static Color gray400 = const Color(0xff797B7F);
+  static Color gray500 = const Color(0xff494A4C);
+  static Color gray550 = const Color(0xff242424);
+  static Color gray600 = const Color(0xff1C1C1C);
+  static Color gray700 = const Color(0xff000000);
+
+  static Color error100 = const Color(0xffF54B31);
+
+  static Color main100 = const Color(0xff88A0FC);
+  static Color main200 = const Color(0xff6686FF);
+}
