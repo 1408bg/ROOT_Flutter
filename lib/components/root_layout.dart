98774803b1beecf0df6b4df7f922496081cff75c
taejun <ptj01036251@gmail.com>
2025-01-07 11:09:24 +0900

    feat: 커스텀 레이아웃 생성

---
+++ lib/components/root_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:root/components/root_colors.dart';
+
+class RootLayout extends StatelessWidget {
+  final PreferredSizeWidget? appBar;
+  final Widget child;
+  final Widget? bottomSheet, bottomNavigationBar;
+
+  const RootLayout({
+    super.key,
+    required this.child,
+    this.appBar,
+    this.bottomSheet,
+    this.bottomNavigationBar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: RootColors.gray600,
+      appBar: appBar,
+      body: child,
+      bottomSheet: bottomSheet,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+}
