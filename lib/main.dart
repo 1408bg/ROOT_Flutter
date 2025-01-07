718b99bdb0d4eab9befd686ad11781ef72fc3be4
taejun <ptj01036251@gmail.com>
2025-01-07 11:12:58 +0900

    debugBanner 삭제하기

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(393, 852),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Pretendard',
           bottomSheetTheme: BottomSheetThemeData(
