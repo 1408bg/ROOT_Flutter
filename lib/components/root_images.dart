4d49ed2ca0d24a10248d1506f55357c4b473b53a
taejun <ptj01036251@gmail.com>
2025-01-07 17:15:49 +0900

    이미지 경로 추가

---
+++ lib/components/root_images.dart
@@ -0,0 +1,21 @@
+class RootImages {
+  RootImages._();
+
+  static const String calender = 'assets/icons/calender.png';
+  static const String eyeOff = 'assets/icons/eye-off.png';
+  static const String eyeOn = 'assets/icons/eye-on.png';
+  static const String home = 'assets/icons/home.png';
+  static const String home2 = 'assets/icons/home.png';
+  static const String bell = 'assets/icons/mynaui_bell.png';
+  static const String paper = 'assets/icons/paper.png';
+  static const String paper2 = 'assets/icons/paper2.png';
+  static const String people = 'assets/icons/people.png';
+  static const String person = 'assets/icons/person.png';
+  static const String person2 = 'assets/icons/person2.png';
+  static const String qrIcon = 'assets/icons/qr_icon.png';
+  static const String qrIcon2 = 'assets/icons/qr_icon2.png';
+  static const String root = 'assets/icons/root.png';
+  static const String location = 'assets/icons/tdesign_location.png';
+  static const String timeLine = 'assets/icons/time-line.png';
+
+}
