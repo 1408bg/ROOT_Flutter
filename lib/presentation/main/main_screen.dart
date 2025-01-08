2abfb7b8f5559840334d0ca57a604665889d40a4
taejun <ptj01036251@gmail.com>
2025-01-08 11:22:53 +0900

    feat: 메인페이지 UI 퍼블리싱

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:root/components/root_layout.dart';
+import 'package:root/presentation/main/widgets/main_screen_app_bar.dart';
 
-class MainScreen extends StatelessWidget {
+import '../../components/root_colors.dart';
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<String> labels = [
+    "봉사 활동 신청",
+    "신입생 2차 전형 면접 도우미",
+    "교내 분리수거 도우미",
+    "교장선생님과 장기 두기 도우미"
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return RootLayout(
+      appBar: const MainScreenAppBar(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 22.0.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 17.h,
+            ),
+            Text(
+              "봉사 활동 신청",
+              style: TextStyle(
+                color: RootColors.gray100,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 17.h,
+            ),
+            SizedBox(
+              height: 650.h,
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      GestureDetector(
+                        child: Container(
+                          width: 353.w,
+                          height: 72.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.r),
+                            border: Border.all(color: RootColors.gray500),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 21.5.w),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  labels[index].toString(),
+                                  style: TextStyle(
+                                      color: RootColors.gray100,
+                                      fontSize: 16.sp,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
