10de4fa7e3b01722141b573432f7efedd679ecd5
taejun <ptj01036251@gmail.com>
2025-01-13 11:51:38 +0900

    feat: 상세 페이지 appbar 구현

---
+++ lib/presentation/main/widgets/detail_app_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:root/components/root_images.dart';
+
+import '../../../components/root_colors.dart';
+
+class DetailAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const DetailAppBar({
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop(context);
+            },
+            icon: Image.asset(
+              RootImages.backIcon,
+              width: 25.w,
+              height: 25.h,
+            ),
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+              color: RootColors.gray100,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(45.h);
+}
