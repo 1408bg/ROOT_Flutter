31f679bc4cc884008dbff54c6686344838639907
taejun <ptj01036251@gmail.com>
2025-03-05 23:15:28 +0900

    feat: firebase 기본 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:root/components/root_colors.dart';
-import 'package:root/presentation/login/login_screen.dart';
+import 'package:root/presentation/user/login_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
